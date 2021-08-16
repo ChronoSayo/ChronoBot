18c6269dc31b44d9d6016b8843a7a6fd591c063a
ChronoSayo <chronosayo@gmail.com>
2021-08-16 21:00:27 +0200

    Remove unnecessary using

--- src/ChronoBot/SocialMedias/Twitter.cs
+++ src/ChronoBot/SocialMedias/Twitter.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 using TweetSharp;
