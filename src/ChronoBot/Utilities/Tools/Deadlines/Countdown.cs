228f1620b4c26860dd0bc927bdaa26ca890b41b0
Syawash Sharafadin <syawash.sharafadin@scania.com>
2023-03-20 14:23:31 +0100

    Fix countdown. Need to fix reminder to do the same and fix twitch's access token class

--- src/ChronoBot/Utilities/Tools/Deadlines/Countdown.cs
+++ src/ChronoBot/Utilities/Tools/Deadlines/Countdown.cs
@@ -4,7 +4,6 @@ using ChronoBot.Helpers;
 using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Timers;
 using ChronoBot.Modules.Tools.Deadlines;
 using ChronoBot.Enums;
