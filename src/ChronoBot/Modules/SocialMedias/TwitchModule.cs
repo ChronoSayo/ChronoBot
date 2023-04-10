914a429acd77ae1cb933bd0d6cef29aa940378e9
ChronoSayo <chronosayo@gmail.com>
2023-04-10 15:33:05 +0200

    Updated twitch to work with new slash

--- src/ChronoBot/Modules/SocialMedias/TwitchModule.cs
+++ src/ChronoBot/Modules/SocialMedias/TwitchModule.cs
@@ -15,5 +15,41 @@ namespace ChronoBot.Modules.SocialMedias
             SocialMedia = socialMedia;
             SocialMediaType = SocialMediaEnum.Twitch;
         }
+
+        [SlashCommand(AddCommand, "Adds streamer to the list of updates.", runMode: RunMode.Async)]
+        public override Task AddSocialMediaUser(
+            [Summary("Streamer", "Insert streamer's name.")] string user,
+            [Summary("Where", "To which channel should this be updated to. Default is this channel.")]
+            [ChannelTypes(ChannelType.Text)] IChannel channel = null)
+        {
+            return base.AddSocialMediaUser(user, channel);
+        }
+
+        [SlashCommand(DeleteCommand, "Deletes streamer from the list of updates.", runMode: RunMode.Async)]
+        public override Task DeleteSocialMediaUser(
+            [Summary("Streamer", "Insert streamer's name.")] string user)
+        {
+            return base.DeleteSocialMediaUser(user);
+        }
+
+        [SlashCommand(GetCommand, "Posts streamer's latest video.", runMode: RunMode.Async)]
+        public override Task GetSocialMediaUser(
+            [Summary("Streamer" , "Insert streamer's name.")]
+            string user)
+        {
+            return base.GetSocialMediaUser(user);
+        }
+
+        [SlashCommand(ListCommand, "Gets a list of added streamers.", runMode: RunMode.Async)]
+        public override Task ListSocialMediaUser()
+        {
+            return base.ListSocialMediaUser();
+        }
+
+        [SlashCommand(UpdateCommand, "Updates all listed streamers in the server.", runMode: RunMode.Async)]
+        public override Task UpdateSocialMediaUser()
+        {
+            return base.UpdateSocialMediaUser();
+        }
     }
 }
